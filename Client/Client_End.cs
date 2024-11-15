97d878025c5a31946f5601d7aeed00d50cdeaa36
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2024-11-15 13:58:41 -0500

    Created basic shell for Client_End class and added to UI.

---
+++ Client/Client_End.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Client
+{
+    internal class Client_End
+    {
+        private void ConnectClient(String server, String message)
+        {
+            try
+            {
+                Int32 port = 13000;
+                TcpClient client = new TcpClient(server, port); //Creates TcpClient
+
+                //Convert information into byte data to send to Server here
+
+                NetworkStream stream = client.GetStream();  //Creating a stream
+
+                //Send data to server here
+
+                Byte[] data = new Byte[256];    //To store server response (game information)
+                String response = String.Empty; //String to store server response
+
+                //Convert response & process it as needed here
+
+                //Close everything
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", ex);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("SocketException: {0}", ex);
+            }
+        }
+    }
+}
